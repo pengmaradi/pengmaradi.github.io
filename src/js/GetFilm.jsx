1988c891df354a21df7898d9a4961b89e65e9905
Xiaoling Peng <pengmaradi@gmail.com>
2024-09-04 11:53:20 +0200

    test: changed react get json data on mobile

--- src/js/GetFilm.jsx
+++ src/js/GetFilm.jsx
@@ -34,19 +34,19 @@ const GetFilm = () => {
 
     return (
         <>
-            <div className="my-5 flex flex-col md:flex-row">
+            <div className="cat-group">
                 {types.map((type) => (
-                <button className="p-2 mb-2 mr-2 w-9 border rounded bg-gray-400 hover:bg-gray-300 visited:bg-gray-300" 
+                <button className="btn" 
                     onClick={getValue}
                     key={type.id}>{type}</button>
                 ))}
             </div>
             <hr className="mb-2" />
-            <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 z-0">
             {data.map((film) => (
-                <li key={film.id} className="pb-2 mb-4">
+                <li key={film.id} className="pb-2 mb-4 duration-1000 transition-all transform" x-intersect="$el.classList.add('fadeInUp')">
                     <h5 className="my-2 text-center">{film.title}</h5>
-                    <img className="w-full h-full sm:w-60 sm:h-auto mb-2 " src={film.posterURL} alt={film.title} />
+                    <img className="" src={film.posterURL} alt={film.title} />
                 </li>
             ))}
             </ul>
